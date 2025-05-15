0b2289da7ae79ee491a8fdacc63401b59eddfb58
Tom Waterbley <tom.waterbley@skyline.be>
2025-05-15 15:25:48 +0200

    Extra unit test for assigning endpoints to virtual signal groups

--- Skyline.DataMiner.MediaOps.Live.Tests/MediaOps_LiveApi_Tests_VirtualSignalGroups.cs
+++ Skyline.DataMiner.MediaOps.Live.Tests/MediaOps_LiveApi_Tests_VirtualSignalGroups.cs
@@ -34,5 +34,24 @@
 				new[] { "Source 1", "Source 2" },
 				vsgs.Select(x => x.Name).ToList());
 		}
+
+		[TestMethod]
+		public void MediaOps_LiveApi_Tests_VirtualSignalGroups_AssignEndpoint()
+		{
+			var vsg = _api.VirtualSignalGroups.Query().First(x => x.Name == "Source 1");
+
+			var videoLevel = _api.Levels.Query().First(x => x.Name == "Video");
+			var videoSource1 = _api.Endpoints.Query().First(x => x.Name == "Video Source 1");
+
+			vsg.Levels.Remove(vsg.Levels.FirstOrDefault(x => x.Level == videoLevel));
+			_api.VirtualSignalGroups.Update(vsg);
+			vsg = _api.VirtualSignalGroups.Query().First(x => x.Name == "Source 1");
+			Assert.AreEqual(1, vsg.Levels.Count);
+
+			vsg.Levels.Add(new API.Objects.LevelEndpoint(videoLevel, videoSource1));
+			_api.VirtualSignalGroups.Update(vsg);
+			vsg = _api.VirtualSignalGroups.Query().First(x => x.Name == "Source 1");
+			Assert.AreEqual(2, vsg.Levels.Count);
+		}
 	}
 }
