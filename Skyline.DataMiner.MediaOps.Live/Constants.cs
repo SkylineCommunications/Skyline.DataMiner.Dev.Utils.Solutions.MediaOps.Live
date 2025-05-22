8ca6af01518f8919ad9dd3e88c6767bccea3a9e0
Tom Waterbley <tom.waterbley@skyline.be>
2025-05-22 11:05:06 +0200

    Make constants public

--- Skyline.DataMiner.MediaOps.Live/Constants.cs
+++ Skyline.DataMiner.MediaOps.Live/Constants.cs
@@ -1,6 +1,6 @@
 ﻿namespace Skyline.DataMiner.MediaOps.Live
 {
-	internal static class Constants
+	public static class Constants
 	{
 		public static string MediationProtocolName => "Skyline MediaOps Mediation";
 	}
