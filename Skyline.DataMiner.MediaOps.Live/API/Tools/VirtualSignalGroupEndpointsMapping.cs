3117193c922f80e949dd6c25aeb955c0b70cacd6
Tom Waterbley <tom.waterbley@skyline.be>
2025-06-04 15:02:20 +0200

    Add VirtualSignalGroupEndpointsMapping

---
+++ Skyline.DataMiner.MediaOps.Live/API/Tools/VirtualSignalGroupEndpointsMapping.cs
@@ -0,0 +1,96 @@
+﻿namespace Skyline.DataMiner.MediaOps.Live.API.Tools
+{
+	using System;
+	using System.Collections.Generic;
+
+	using Skyline.DataMiner.MediaOps.Live.API.Objects;
+	using Skyline.DataMiner.MediaOps.Live.Tools;
+
+	public class VirtualSignalGroupEndpointsMapping
+	{
+		private readonly ManyToManyMapping<VirtualSignalGroup, ApiObjectReference<Endpoint>> _mapping = new ManyToManyMapping<VirtualSignalGroup, ApiObjectReference<Endpoint>>();
+
+		public int VirtualSignalGroupCount => _mapping.Forward.Count;
+
+		public int EndpointCount => _mapping.Reverse.Count;
+
+		public bool TryGetEndpoints(VirtualSignalGroup virtualSignalGroup, out ICollection<ApiObjectReference<Endpoint>> endpoints)
+		{
+			if (virtualSignalGroup is null)
+			{
+				throw new ArgumentNullException(nameof(virtualSignalGroup));
+			}
+
+			return _mapping.Forward.TryGetValue(virtualSignalGroup, out endpoints);
+		}
+
+		public bool TryGetVirtualSignalGroups(ApiObjectReference<Endpoint> endpoint, out ICollection<VirtualSignalGroup> virtualSignalGroups)
+		{
+			return _mapping.Reverse.TryGetValue(endpoint, out virtualSignalGroups);
+		}
+
+		public void Add(VirtualSignalGroup virtualSignalGroup)
+		{
+			if (virtualSignalGroup is null)
+			{
+				throw new ArgumentNullException(nameof(virtualSignalGroup));
+			}
+
+			foreach (var levelEndpoint in virtualSignalGroup.GetEndpoints())
+			{
+				_mapping.Add(virtualSignalGroup, levelEndpoint.Endpoint);
+			}
+		}
+
+		public void Remove(VirtualSignalGroup virtualSignalGroup)
+		{
+			if (virtualSignalGroup is null)
+			{
+				throw new ArgumentNullException(nameof(virtualSignalGroup));
+			}
+
+			_mapping.RemoveForward(virtualSignalGroup);
+		}
+
+		public void Update(VirtualSignalGroup virtualSignalGroup)
+		{
+			if (virtualSignalGroup is null)
+			{
+				throw new ArgumentNullException(nameof(virtualSignalGroup));
+			}
+
+			Remove(virtualSignalGroup);
+			Add(virtualSignalGroup);
+		}
+
+		public void Clear()
+		{
+			_mapping.Clear();
+		}
+
+		public bool Contains(VirtualSignalGroup virtualSignalGroup)
+		{
+			if (virtualSignalGroup is null)
+			{
+				throw new ArgumentNullException(nameof(virtualSignalGroup));
+			}
+
+			return _mapping.Forward.ContainsKey(virtualSignalGroup);
+		}
+
+		public bool Contains(ApiObjectReference<Endpoint> endpoint)
+		{
+			return _mapping.Reverse.ContainsKey(endpoint);
+		}
+
+		public bool Contains(VirtualSignalGroup virtualSignalGroup, ApiObjectReference<Endpoint> endpoint)
+		{
+			if (virtualSignalGroup is null)
+			{
+				throw new ArgumentNullException(nameof(virtualSignalGroup));
+			}
+
+			return _mapping.Contains(virtualSignalGroup, endpoint);
+		}
+	}
+}
---
+++ Skyline.DataMiner.MediaOps.Live/Tools/ManyToManyMapping.cs
@@ -0,0 +1,152 @@
+﻿namespace Skyline.DataMiner.MediaOps.Live.Tools
+{
+	using System;
+	using System.Collections.Generic;
+
+	internal class ManyToManyMapping<Ta, Tb>
+	{
+		#region Private Fields
+
+		private readonly Dictionary<Ta, ICollection<Tb>> _forwardMapping = new Dictionary<Ta, ICollection<Tb>>();
+		private readonly Dictionary<Tb, ICollection<Ta>> _reverseMapping = new Dictionary<Tb, ICollection<Ta>>();
+
+		#endregion
+
+		#region Public Properties
+
+		public IDictionary<Ta, ICollection<Tb>> Forward
+		{
+			get { return _forwardMapping; }
+		}
+
+		public IDictionary<Tb, ICollection<Ta>> Reverse
+		{
+			get { return _reverseMapping; }
+		}
+
+		public int Count
+		{
+			get { return _forwardMapping.Count; }
+		}
+
+		#endregion
+
+		#region Public Methods
+
+		public void Add(Ta a, Tb b)
+		{
+			if (a == null)
+				throw new ArgumentNullException("a", "Key cannot be null");
+
+			if (b == null)
+				throw new ArgumentNullException("b", "Key cannot be null");
+
+			if (!_forwardMapping.TryGetValue(a, out var listA))
+			{
+				listA = new List<Tb>() { b };
+				_forwardMapping.Add(a, listA);
+			}
+			else
+			{
+				if (listA.Contains(b))
+					throw new ArgumentException("Item already exists", "b");
+				listA.Add(b);
+			}
+
+			if (!_reverseMapping.TryGetValue(b, out var listB))
+			{
+				listB = new List<Ta>() { a };
+				_reverseMapping.Add(b, listB);
+			}
+			else
+			{
+				if (listB.Contains(a))
+					throw new ArgumentException("Item already exists", "a");
+				listB.Add(a);
+			}
+		}
+
+		public void Remove(Ta a, Tb b)
+		{
+			if (a == null)
+				throw new ArgumentNullException("a", "Key cannot be null");
+
+			if (b == null)
+				throw new ArgumentNullException("b", "Key cannot be null");
+
+			if (!_forwardMapping.TryGetValue(a, out var listA))
+				throw new ArgumentException("Key does not exist", "a");
+
+			if (!_reverseMapping.TryGetValue(b, out var listB))
+				throw new ArgumentException("Key does not exist", "b");
+
+			listA.Remove(b);
+			if (listA.Count == 0)
+				_forwardMapping.Remove(a);
+
+			listB.Remove(a);
+			if (listB.Count == 0)
+				_reverseMapping.Remove(b);
+		}
+
+		public void RemoveForward(Ta a)
+		{
+			if (a == null)
+				throw new ArgumentNullException("a", "Key cannot be null");
+
+			if (!_forwardMapping.TryGetValue(a, out var b))
+				throw new ArgumentException("Key does not exist", "a");
+
+			_forwardMapping.Remove(a);
+
+			foreach (var x in b)
+			{
+				var list = _reverseMapping[x];
+				list.Remove(a);
+				if (list.Count == 0)
+					_reverseMapping.Remove(x);
+			}
+		}
+
+		public void RemoveReverse(Tb b)
+		{
+			if (b == null)
+				throw new ArgumentNullException("b", "Key cannot be null");
+
+			if (!_reverseMapping.TryGetValue(b, out var a))
+				throw new ArgumentException("Key does not exist", "b");
+
+			foreach (var x in a)
+			{
+				var list = _forwardMapping[x];
+				list.Remove(b);
+				if (list.Count == 0)
+					_forwardMapping.Remove(x);
+			}
+
+			_reverseMapping.Remove(b);
+		}
+
+		public void Clear()
+		{
+			_forwardMapping.Clear();
+			_reverseMapping.Clear();
+		}
+
+		public bool Contains(Ta a, Tb b)
+		{
+			if (a == null)
+				throw new ArgumentNullException("a", "Key cannot be null");
+
+			if (b == null)
+				throw new ArgumentNullException("b", "Key cannot be null");
+
+			if (!_forwardMapping.TryGetValue(a, out var list))
+				return false;
+
+			return list.Contains(b);
+		}
+
+		#endregion
+	}
+}
