0f626f0b37ae6e735b1b383b02caa0931f2163d1
Tom Waterbley <tom.waterbley@skyline.be>
2025-06-06 11:47:19 +0200

    big cleanup

--- Skyline.DataMiner.MediaOps.Live/API/Connectivity/ConnectionStatus.cs
+++ Skyline.DataMiner.MediaOps.Live/API/Connectivity/ConnectionStatus.cs
@@ -2,8 +2,19 @@
 {
 	public enum ConnectionStatus
 	{
+		/// <summary>
+		/// None of the endpoints are connected.
+		/// </summary>
 		Disconnected,
+
+		/// <summary>
+		/// Some, but not all, endpoints are connected.
+		/// </summary>
 		Partial,
+
+		/// <summary>
+		/// All endpoints are connected.
+		/// </summary>
 		Connected,
 	}
 }
