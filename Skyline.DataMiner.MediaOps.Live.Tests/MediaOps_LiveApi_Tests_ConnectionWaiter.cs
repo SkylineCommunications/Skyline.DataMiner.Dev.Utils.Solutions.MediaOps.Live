d9f250c6eeb325fd8e7e6307a2ecbfd6489d558a
Tom Waterbley <tom.waterbley@skyline.be>
2025-07-01 16:07:45 +0200

    Connection waiter

---
+++ Skyline.DataMiner.MediaOps.Live.Tests/MediaOps_LiveApi_Tests_ConnectionWaiter.cs
@@ -0,0 +1,67 @@
+﻿namespace Skyline.DataMiner.MediaOps.Live.Tests
+{
+	using Skyline.DataMiner.MediaOps.Live.API.Connectivity;
+	using Skyline.DataMiner.MediaOps.Live.UnitTesting;
+
+	[TestClass]
+	public sealed class MediaOps_LiveApi_Tests_ConnectionWaiter
+	{
+		[TestMethod]
+		public void MediaOps_LiveApi_Tests_ConnectionWaiter_WaitUntilConnected_VirtualSignalGroup()
+		{
+			var simulation = new MediaOpsLiveSimulation();
+			var api = simulation.Api;
+
+			var audioSource1 = api.Endpoints.Read("Audio Source 1");
+			var videoSource1 = api.Endpoints.Read("Video Source 1");
+			var audioDestination1 = api.Endpoints.Read("Audio Destination 1");
+			var videoDestination1 = api.Endpoints.Read("Video Destination 1");
+
+			var source1 = api.VirtualSignalGroups.Read("Source 1");
+			var destination1 = api.VirtualSignalGroups.Read("Destination 1");
+
+			using (var connectivity = new ConnectivityInfoProvider(api))
+			{
+				var waitTask = Task.Run(() =>
+					ConnectionWaiter.WaitUntilConnected(connectivity, source1, destination1, TimeSpan.FromMinutes(1)));
+
+				Thread.Sleep(200);
+				Assert.IsFalse(waitTask.IsCompleted);
+
+				simulation.CreateTestConnection(videoSource1, videoDestination1);
+				simulation.CreateTestConnection(audioSource1, audioDestination1);
+
+				waitTask.Wait(TimeSpan.FromSeconds(5));
+
+				Assert.IsTrue(waitTask.IsCompleted);
+				Assert.IsTrue(waitTask.Result);
+			}
+		}
+
+		[TestMethod]
+		public void MediaOps_LiveApi_Tests_ConnectionWaiter_WaitUntilConnected_Endpoint()
+		{
+			var simulation = new MediaOpsLiveSimulation();
+			var api = simulation.Api;
+
+			var videoSource1 = api.Endpoints.Read("Video Source 1");
+			var videoDestination1 = api.Endpoints.Read("Video Destination 1");
+
+			using (var connectivity = new ConnectivityInfoProvider(api))
+			{
+				var waitTask = Task.Run(() =>
+					ConnectionWaiter.WaitUntilConnected(connectivity, videoSource1, videoDestination1, TimeSpan.FromMinutes(1)));
+
+				Thread.Sleep(200);
+				Assert.IsFalse(waitTask.IsCompleted);
+
+				simulation.CreateTestConnection(videoSource1, videoDestination1);
+
+				waitTask.Wait(TimeSpan.FromSeconds(5));
+
+				Assert.IsTrue(waitTask.IsCompleted);
+				Assert.IsTrue(waitTask.Result);
+			}
+		}
+	}
+}
---
+++ Skyline.DataMiner.MediaOps.Live/API/Connectivity/ConnectionWaiter.cs
@@ -0,0 +1,116 @@
+﻿namespace Skyline.DataMiner.MediaOps.Live.API.Connectivity
+{
+	using System;
+	using System.Linq;
+	using System.Threading.Tasks;
+
+	using Skyline.DataMiner.MediaOps.Live.API.Objects.ConnectivityManagement;
+
+	public static class ConnectionWaiter
+	{
+		public static bool WaitUntilConnected(ConnectivityInfoProvider connectivityInfoProvider, VirtualSignalGroup source, VirtualSignalGroup destination, TimeSpan timeout)
+		{
+			if (connectivityInfoProvider == null)
+			{
+				throw new ArgumentNullException(nameof(connectivityInfoProvider));
+			}
+
+			if (source == null)
+			{
+				throw new ArgumentNullException(nameof(source));
+			}
+
+			if (destination == null)
+			{
+				throw new ArgumentNullException(nameof(destination));
+			}
+
+			var tsc = new TaskCompletionSource<bool>();
+
+			EventHandler<ConnectionsUpdatedEvent> connectionEventHandler = (s, e) =>
+			{
+				foreach (var connectivity in e.VirtualSignalGroups)
+				{
+					if (connectivity.VirtualSignalGroup == destination &&
+						connectivity.ConnectedSources.Contains(source))
+					{
+						tsc.TrySetResult(true);
+						return;
+					}
+				}
+			};
+
+			connectivityInfoProvider.Subscribe();
+			connectivityInfoProvider.ConnectionsUpdated += connectionEventHandler;
+
+			try
+			{
+				var currentConnectivity = connectivityInfoProvider.GetConnectivity(destination);
+
+				if (currentConnectivity.ConnectedSources.Contains(source))
+				{
+					tsc.TrySetResult(true);
+				}
+
+				return tsc.Task.Wait(timeout);
+			}
+			finally
+			{
+				connectivityInfoProvider.ConnectionsUpdated -= connectionEventHandler;
+			}
+		}
+
+		public static bool WaitUntilConnected(ConnectivityInfoProvider connectivityInfoProvider, Endpoint source, Endpoint destination, TimeSpan timeout)
+		{
+			if (connectivityInfoProvider == null)
+			{
+				throw new ArgumentNullException(nameof(connectivityInfoProvider));
+			}
+
+			if (source == null)
+			{
+				throw new ArgumentNullException(nameof(source));
+			}
+
+			if (destination == null)
+			{
+				throw new ArgumentNullException(nameof(destination));
+			}
+
+			var tsc = new TaskCompletionSource<bool>();
+
+			EventHandler<ConnectionsUpdatedEvent> connectionEventHandler = (s, e) =>
+			{
+				foreach (var connectivity in e.Endpoints)
+				{
+					if (connectivity.Endpoint == destination &&
+						connectivity.ConnectedSource?.Endpoint == source)
+					{
+						tsc.TrySetResult(true);
+						return;
+					}
+				}
+			};
+
+			connectivityInfoProvider.Subscribe();
+			connectivityInfoProvider.ConnectionsUpdated += connectionEventHandler;
+
+			try
+			{
+				var currentConnectivity = connectivityInfoProvider.GetConnectivity(destination);
+
+				if (currentConnectivity.ConnectedSource?.Endpoint == source)
+				{
+					tsc.TrySetResult(true);
+				}
+
+				return tsc.Task.Wait(timeout);
+			}
+			finally
+			{
+				connectivityInfoProvider.ConnectionsUpdated -= connectionEventHandler;
+			}
+		}
+
+	}
+}
