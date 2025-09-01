b84a86058d6a32f21be8f2995ad1c0126140d630
Tom Waterbley <tom.waterbley@skyline.be>
2025-09-01 12:02:38 +0200

    Fix dispose remark

--- Skyline.DataMiner.MediaOps.Live/API/Caching/VirtualSignalGroupEndpointsCache.cs
+++ Skyline.DataMiner.MediaOps.Live/API/Caching/VirtualSignalGroupEndpointsCache.cs
@@ -110,7 +110,13 @@
 			}
 		}
 
-		public virtual void Dispose()
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+
+		protected virtual void Dispose(bool disposing)
 		{
 			Unsubscribe();
 		}
