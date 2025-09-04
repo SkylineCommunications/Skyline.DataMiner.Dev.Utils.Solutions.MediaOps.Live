9883e2e439b3a72666de1274238c8278ffc24d45
Tom Waterbley <tom.waterbley@skyline.be>
2025-09-04 08:13:29 +0200

    Remove all predefined transport types except TSoIP

--- Skyline.DataMiner.MediaOps.Live/API/Repositories/PredefinedTransportTypes.cs
+++ Skyline.DataMiner.MediaOps.Live/API/Repositories/PredefinedTransportTypes.cs
@@ -8,22 +8,12 @@
 
 	public static class PredefinedTransportTypes
 	{
-		public static TransportType IP { get; } = new TransportType(Guid.Parse("7d8e541b-4e74-4973-a700-9ca352aa8c0b")) { Name = "IP" };
-
-		public static TransportType SDI { get; } = new TransportType(Guid.Parse("858b9804-269c-43be-bac2-79b20ef4bc61")) { Name = "SDI" };
-
 		public static TransportType TSoIP { get; } = new TransportType(Guid.Parse("37f7faf4-6786-429d-9d66-6e46662c1986")) { Name = "TSoIP" };
 
-		public static TransportType SRT { get; } = new TransportType(Guid.Parse("18c3f4ed-6693-4652-b792-795773833f9c")) { Name = "SRT" };
-
 		public static TransportType[] All { get; } =
-			new[]
-			{
-				IP,
-				SDI,
-				TSoIP,
-				SRT,
-			};
+		[
+			TSoIP,
+		];
 
 		public static IReadOnlyDictionary<Guid, TransportType> ById { get; } = All.ToDictionary(x => x.ID);
 
