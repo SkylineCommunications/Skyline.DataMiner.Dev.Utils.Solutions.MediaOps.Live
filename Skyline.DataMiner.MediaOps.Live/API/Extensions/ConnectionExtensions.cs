86b9dd4c97671121a4029a6719a3081285d6a31d
Tom Waterbley <tom.waterbley@skyline.be>
2025-09-18 14:58:24 +0200

    GetMediaOpsLiveApi extension method on IConnection

---
+++ Skyline.DataMiner.MediaOps.Live/API/Extensions/ConnectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace Skyline.DataMiner.MediaOps.Live.API.Extensions
+{
+	using System;
+
+	using Skyline.DataMiner.Net;
+
+	public static class ConnectionExtensions
+	{
+		public static MediaOpsLiveApi GetMediaOpsLiveApi(this IConnection connection)
+		{
+			if (connection is null)
+			{
+				throw new ArgumentNullException(nameof(connection));
+			}
+
+			return new MediaOpsLiveApi(connection);
+		}
+	}
+}
