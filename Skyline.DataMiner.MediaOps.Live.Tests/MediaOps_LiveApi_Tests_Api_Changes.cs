7db68864e0daf93abd7b222a879a8162a02134e0
Tom Waterbley <tom.waterbley@skyline.be>
2025-09-18 16:41:50 +0200

    Add detection of public changes in the API

---
+++ Skyline.DataMiner.MediaOps.Live.Tests/MediaOps_LiveApi_Tests_Api_Changes.cs
@@ -0,0 +1,51 @@
+﻿namespace Skyline.DataMiner.MediaOps.Live.Tests
+{
+	using System.Reflection;
+
+	using PublicApiGenerator;
+
+	[TestClass]
+	[UsesVerify]
+	public sealed partial class MediaOps_LiveApi_Tests_Api_PublicChanges
+	{
+		[TestMethod]
+		public Task MediaOps_LiveApi_Tests_Api_NoPublicApiChanges_Common()
+		{
+			var assemblyName = "Skyline.DataMiner.MediaOps.Live";
+			var publicApi = Assembly.Load(assemblyName).GeneratePublicApi();
+
+			return Verifier.Verify(publicApi)
+				.UseFileName($"{assemblyName}_PublicApi");
+		}
+
+		[TestMethod]
+		public Task MediaOps_LiveApi_Tests_Api_NoPublicApiChanges_Automation()
+		{
+			var assemblyName = "Skyline.DataMiner.MediaOps.Live.Automation";
+			var publicApi = Assembly.Load(assemblyName).GeneratePublicApi();
+
+			return Verifier.Verify(publicApi)
+				.UseFileName($"{assemblyName}_PublicApi");
+		}
+
+		[TestMethod]
+		public Task MediaOps_LiveApi_Tests_Api_NoPublicApiChanges_Protocol()
+		{
+			var assemblyName = "Skyline.DataMiner.MediaOps.Live.Protocol";
+			var publicApi = Assembly.Load(assemblyName).GeneratePublicApi();
+
+			return Verifier.Verify(publicApi)
+				.UseFileName($"{assemblyName}_PublicApi");
+		}
+
+		[TestMethod]
+		public Task MediaOps_LiveApi_Tests_Api_NoPublicApiChanges_GQI()
+		{
+			var assemblyName = "Skyline.DataMiner.MediaOps.Live.GQI";
+			var publicApi = Assembly.Load(assemblyName).GeneratePublicApi();
+
+			return Verifier.Verify(publicApi)
+				.UseFileName($"{assemblyName}_PublicApi");
+		}
+	}
+}
