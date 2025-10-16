a58ecc39b4ebb17938c6a46aa7c1b4beb8cf6739
Tom Waterbley <tom.waterbley@skyline.be>
2025-10-16 14:57:02 +0200

    Update

--- Skyline.DataMiner.MediaOps.Live/DOM/Model/EndpointInstance.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Skyline.DataMiner.MediaOps.Live.DOM.Model.SlcConnectivityManagement
-{
-	public partial class EndpointInstance
-	{
-		public bool IsSource => EndpointInfo.Role == SlcConnectivityManagementIds.Enums.Role.Source;
-
-		public bool IsDestination => EndpointInfo.Role == SlcConnectivityManagementIds.Enums.Role.Destination;
-	}
-}
