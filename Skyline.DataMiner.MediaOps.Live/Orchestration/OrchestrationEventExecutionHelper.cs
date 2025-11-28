74b9fdf135461f7af184c286eb5ac9765a1ce9c6
robin_spruytte <robin.spruytte@skyline.be>
2025-11-28 16:17:25 +0100

    Added disconnect request with metadata

--- Skyline.DataMiner.MediaOps.Live/Orchestration/OrchestrationEventExecutionHelper.cs
+++ Skyline.DataMiner.MediaOps.Live/Orchestration/OrchestrationEventExecutionHelper.cs
@@ -250,7 +250,11 @@
 				}
 				catch (DisconnectFailedException e)
 				{
-					IEnumerable<string> eventsForFailedRequests = e.FailedRequests.Select(fail => Convert.ToString(fail.MetaData));
+
+					IEnumerable<string> eventsForFailedRequests = e.FailedRequests
+						.OfType<VsgDisconnectRequestWithMetadata>()
+						.Select(fail => Convert.ToString(fail.MetaData));
+
 					foreach (OrchestrationEventConfiguration orchestrationEventConfiguration in eventConfigurations.Where(eventConfig => eventsForFailedRequests.Contains(eventConfig.ID.ToString())))
 					{
 						orchestrationEventConfiguration.InternalSetState(EventState.Failed);
@@ -315,14 +319,14 @@
 
 						if (connection.LevelMappings == null || !connection.LevelMappings.Any())
 						{
-							requests.Add(new VsgDisconnectRequest(dstVirtualSignalGroup)
+							requests.Add(new VsgDisconnectRequestWithMetadata(dstVirtualSignalGroup)
 							{
 								MetaData = eventId.ToString(),
 							});
 							continue;
 						}
 
-						requests.Add(new VsgDisconnectRequest(dstVirtualSignalGroup, Enumerable.ToHashSet(allInvolvedLevels.Select(level => new ApiObjectReference<Level>(level.ID))))
+						requests.Add(new VsgDisconnectRequestWithMetadata(dstVirtualSignalGroup, Enumerable.ToHashSet(allInvolvedLevels.Select(level => new ApiObjectReference<Level>(level.ID))))
 						{
 							MetaData = eventId.ToString(),
 						});
---
+++ Skyline.DataMiner.MediaOps.Live/Orchestration/VsgDisconnectRequestWithMetadata.cs
@@ -0,0 +1,18 @@
+﻿namespace Skyline.DataMiner.MediaOps.Live.Orchestration
+{
+	using System.Collections.Generic;
+
+	using Skyline.DataMiner.MediaOps.Live.API.Objects;
+	using Skyline.DataMiner.MediaOps.Live.API.Objects.ConnectivityManagement;
+	using Skyline.DataMiner.MediaOps.Live.Take;
+
+	internal class VsgDisconnectRequestWithMetadata : VsgDisconnectRequest
+	{
+		public VsgDisconnectRequestWithMetadata(VirtualSignalGroup destination, ICollection<ApiObjectReference<Level>> levels = null)
+			: base(destination, levels)
+		{
+		}
+
+		public object MetaData { get; set; }
+	}
+}
