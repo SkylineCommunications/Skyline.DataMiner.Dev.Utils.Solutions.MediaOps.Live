54ae142aca4e736455f71fbe7c7683a48fa7e9e4
Tom Waterbley <tom.waterbley@skyline.be>
2025-12-15 10:09:32 +0100

    Simple test for interactive script

---
+++ Skyline.DataMiner.MediaOps.Live.Tests/MediaOps_Live_Tests_Interactive.cs
@@ -0,0 +1,18 @@
+﻿namespace Skyline.DataMiner.MediaOps.Live.Tests
+{
+	using FluentAssertions;
+
+	using Skyline.DataMiner.Automation;
+
+	[TestClass]
+	public sealed class MediaOps_Live_Tests_Interactive
+	{
+		[TestMethod]
+		public void MediaOps_Live_Tests_Interactive_Test()
+		{
+			var uib = new UIBuilder();
+
+			uib.Should().NotBeNull();
+		}
+	}
+}
