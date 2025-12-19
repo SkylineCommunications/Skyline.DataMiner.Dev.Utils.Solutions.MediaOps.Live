c90fe224df47c0ee581862fd726c0215090d7d0b
robin_spruytte <robin.spruytte@skyline.be>
2025-12-19 15:12:13 +0100

    Add buffer mechanism to DOM writes of events

---
+++ Skyline.DataMiner.MediaOps.Live/API/Repositories/WriteBuffer.cs
@@ -0,0 +1,71 @@
+﻿namespace Skyline.DataMiner.MediaOps.Live.API.Repositories
+{
+	using System;
+	using System.Collections.Concurrent;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Timers;
+
+	using Skyline.DataMiner.MediaOps.Live.API.Objects;
+
+	internal class WriteBuffer<T> : IDisposable where T : ApiObject<T>
+	{
+		private readonly Repository<T> _repository;
+		private readonly ConcurrentQueue<T> _queue;
+		private readonly Timer _timer;
+
+		public WriteBuffer(Repository<T> repository) : this(repository, TimeSpan.FromSeconds(1))
+		{
+		}
+
+		public WriteBuffer(Repository<T> repository, TimeSpan writeInterval)
+		{
+			_repository = repository ?? throw new ArgumentNullException(nameof(repository));
+			_queue = new ConcurrentQueue<T>();
+
+			_timer = new Timer(writeInterval.Milliseconds);
+			_timer.Elapsed += IntervalPassed;
+			_timer.AutoReset = true;
+			_timer.Start();
+		}
+
+		public void QueueItems(IEnumerable<T> items)
+		{
+			foreach (T item in items)
+			{
+				_queue.Enqueue(item);
+			}
+		}
+
+		public void Queue(T item)
+		{
+			_queue.Enqueue(item);
+		}
+
+		private void IntervalPassed(object sender, ElapsedEventArgs e)
+		{
+			try
+			{
+				List<T> items = new List<T>();
+				while (_queue.TryDequeue(out T item))
+				{
+					items.Add(item);
+				}
+
+				if (items.Any())
+				{
+					_repository.CreateOrUpdate(items);
+				}
+			}
+			catch (Exception)
+			{
+				// Nothing
+			}
+		}
+
+		public void Dispose()
+		{
+			_timer?.Dispose();
+		}
+	}
+}
