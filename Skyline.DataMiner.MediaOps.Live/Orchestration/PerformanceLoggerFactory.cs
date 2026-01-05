3072deb09e3367467abd0bbe356b6c82395ce5ad
Tom Waterbley <tom.waterbley@skyline.be>
2026-01-05 08:25:02 +0100

    Don't create files in unit tests (#47)

---
+++ Skyline.DataMiner.MediaOps.Live/Orchestration/PerformanceLoggerFactory.cs
@@ -0,0 +1,22 @@
+﻿namespace Skyline.DataMiner.MediaOps.Live.Orchestration
+{
+	using System;
+
+	using Skyline.DataMiner.MediaOps.Live.Logging;
+	using Skyline.DataMiner.MediaOps.Live.Tools;
+	using Skyline.DataMiner.Utils.PerformanceAnalyzer.Loggers;
+
+	internal static class PerformanceLoggerFactory
+	{
+		internal static IPerformanceLogger Create(string methodName)
+		{
+			if (UnitTestDetector.IsInUnitTest)
+			{
+				return new NullPerformanceLogger();
+			}
+
+			string performanceLogFilename = $"ORC-API - {DateTime.UtcNow:yyyy-MM-dd}";
+			return new PerformanceFileLogger(methodName, performanceLogFilename);
+		}
+	}
+}
